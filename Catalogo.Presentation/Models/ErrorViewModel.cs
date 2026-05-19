4fac4c827fe1fc80785fe5a3fb40096f8796503f
Noe Euan Herrera <noe.euan@tecdesoftware.edu.mx>
2026-05-19 12:52:14 -0600

    Agregar archivos de proyecto.

---
+++ Catalogo.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Catalogo.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
